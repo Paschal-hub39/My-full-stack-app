202cbaff3334663c9b65c0540cda62a7bb513d08
Paschal-hub39 <azubuikepaschal6@gmail.com>
2026-03-27 23:43:17 +0100

    Update SW.js

--- SW.js
+++ SW.js
@@ -1,10 +1,41 @@
+// CACHE NAME
 const cacheName = 'paschala-v1';
-const assets = ['/', 'index.html', 'style.css', 'script.js', '391920b5313be4a92336a4b7a0dd3e54.jpg'];
+const assets = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/391920b5313be4a92336a4b7a0dd3e54.jpg',
+    '/manifest.json'
+];
 
+// INSTALL SERVICE WORKER
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+    e.waitUntil(
+        caches.open(cacheName)
+            .then(cache => cache.addAll(assets))
+            .then(() => self.skipWaiting())
+    );
 });
 
+// ACTIVATE SERVICE WORKER & CLEAN OLD CACHES
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => {
+                    if(key !== cacheName) return caches.delete(key);
+                })
+            )
+        )
+    );
+    self.clients.claim();
+});
+
+// FETCH EVENT - SERVE CACHE FIRST
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+    e.respondWith(
+        caches.match(e.request)
+            .then(res => res || fetch(e.request).catch(() => caches.match('/index.html')))
+    );
 });
